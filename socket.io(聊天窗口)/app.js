0cd5a86d3285deb9abed41dbf063183b41415088
Yuhan zhong <715181149@qq.com>
2018-07-18 16:22:50 +0800

    Update app.js

--- socket.io(聊天窗口)/app.js
+++ socket.io(聊天窗口)/app.js
@@ -1,7 +1,7 @@
 /*所有第三方的模块引入*/
 const express = require("express");
 const logger = require("morgan"); //日志
-const favicon = require("serve-favicon");//小图标
+const favicon = require("serve-favicon");// 小图标
 const bodyParser = require("body-parser");  //处理POST 数据
 const path = require("path"); //处理路径
 
