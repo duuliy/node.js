44fd9bd83bca3ee70d9e9a72cc28f6285f27bd84
Yuhan zhong <715181149@qq.com>
2018-10-24 16:33:49 +0800

    Update app.js

--- session,cookie/app.js
+++ session,cookie/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path"); //处理路径
 const router = express.Router();
 const app = express();
-var signature = require('cookie-signature');
+var signature = require('cookie-signature');   //去掉cookies的签名
 
 
 
@@ -64,6 +64,14 @@ app.use("/", (req, resp, next) => {
 //静态资源
 app.use(express.static(__dirname + "/public"));
 
+ //domain：cookie在什么域名下有效，类型为String,。默认为网站域名
+   //expires: cookie过期时间，类型为Date。如果没有设置或者设置为0，那么该cookie只在这个这个session有效，即关闭浏览器后，这个cookie会被浏览器删除。
+   //httpOnly: 只能被web server访问，类型Boolean。
+   //maxAge: 实现expires的功能，设置cookie过期的时间，类型为String，指明从现在开始，多少毫秒以后，cookie到期。
+   //path: cookie在什么路径下有效，默认为'/'，类型为String
+   //secure：只能被HTTPS使用，类型Boolean，默认为false
+   //signed:使用签名，类型Boolean，默认为false。`express会使用req.secret来完成签名，需要cookie-parser配合使用`
+
 
 
 router.post("/newLogin.do", (req, resp) => {
@@ -77,7 +85,7 @@ router.post("/newLogin.do", (req, resp) => {
         }
         resp.cookie('islogin', username, {
             maxAge: 60 * 1000,
-            signed: true,
+            signed: true,  //使用签名
             httpOnly: true
         });
         resp.send(data)
