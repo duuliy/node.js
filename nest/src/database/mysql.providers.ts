4d95225d9f0d47a90eb69f1ee77814aa40aa6d61
duuliy <715181149@qq.com>
2020-07-23 23:50:38 +0800

    chore: nest mac

--- nest/src/database/mysql.providers.ts
+++ nest/src/database/mysql.providers.ts
@@ -4,17 +4,16 @@ import { createConnection } from 'typeorm';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: async () => await createConnection({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'springboottest',
-      entities: [
-          __dirname + '/../**/*.entity{.ts,.js}',
-      ],
-      synchronize: true,
-    }),
+    useFactory: async () =>
+      await createConnection({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'root',
+        database: 'springboottest',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
   },
 ];
--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,14 @@
 // // 应用程序入口文件。
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
-import { AllExceptionsFilter,HttpExceptionFilter } from './filtter/httpException.filter';
-import { ExpressAdapter,NestExpressApplication } from '@nestjs/platform-express';
+import {
+  AllExceptionsFilter,
+  HttpExceptionFilter,
+} from './filtter/httpException.filter';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 import { ValidationPipe } from './pipe/validation.pipe';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
@@ -11,14 +17,13 @@ import * as helmet from 'helmet';
 import * as csurf from 'csurf';
 import * as rateLimit from 'express-rate-limit';
 
-
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const https = require('https');
 const http = require('http');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 // const redis = require("redis");   //https和缓存都运维来
 // const client = redis.createClient();
 
@@ -37,29 +42,31 @@ const cookieParser = require('cookie-parser')
 //     client.quit();
 // });
 
-
-const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, '/secrets/privkey.key')),
-  cert: fs.readFileSync(__dirname + '/secrets/CERTIFICATE.crt'),
-};
+// const httpsOptions = {
+//   key: fs.readFileSync(path.join(__dirname, '/secrets/privkey.key')),
+//   cert: fs.readFileSync(__dirname + '/secrets/CERTIFICATE.crt'),
+// };
 
 const server = express();
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, 
-  new ExpressAdapter(server),{
-    // logger: ['error', 'warn']  //日志类型
-  });
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+    {
+      // logger: ['error', 'warn']  //日志类型
+    },
+  );
   //混合应用以下全局设置无效 需要在module中设置
   // app.use(logger);  //全局中间件使用方式
-  app.useStaticAssets(join(__dirname, '..', 'public'),{
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
     prefix: '/static/', //设置虚拟路径
-  }) // http://localhost:3000/static/xxx.html
+  }); // http://localhost:3000/static/xxx.html
   app.useGlobalFilters(new HttpExceptionFilter()); //全局
   app.setGlobalPrefix('v1'); //url前缀
   app.useGlobalPipes(new ValidationPipe()); //全局类型错误监测
   // app.useGlobalGuards(new RolesGuard());  //全局守卫
-  app.use(compression())  //压缩代码
-  app.use(helmet())  //过适当地设置 HTTP 头，保护您的应用免受一些众所周知的 Web 漏洞的影响  安全
+  app.use(compression()); //压缩代码
+  app.use(helmet()); //过适当地设置 HTTP 头，保护您的应用免受一些众所周知的 Web 漏洞的影响  安全
   // app.enableCors()  //允许跨域
   // app.use(bodyParser.urlencoded({ extended: false }))
   // app.use(cookieParser())
@@ -69,15 +76,15 @@ async function bootstrap() {
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 100, // 每个窗口最多限制100个请求
     }),
-  );  //限速防止暴力攻击
+  ); //限速防止暴力攻击
 
-  app.use(function (err, req, res, next) {
-    if (err.code !== 'EBADCSRFTOKEN') return next(err)
-  
+  app.use(function(err, req, res, next) {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
     // handle CSRF token errors here
-    res.status(403)
-    res.send('form tampered with')
-  })
+    res.status(403);
+    res.send('form tampered with');
+  });
 
   //swgger  http://localhost:3000/api/
   const options = new DocumentBuilder()
@@ -93,8 +100,7 @@ async function bootstrap() {
 }
 bootstrap();
 http.createServer(server).listen(3000);
-https.createServer(httpsOptions, server).listen(443);
-
+// https.createServer(httpsOptions, server).listen(443);
 
 // import { ValidationPipe } from '@nestjs/common';
 // import { NestFactory } from '@nestjs/core';
@@ -105,7 +111,3 @@ https.createServer(httpsOptions, server).listen(443);
 //   await app.listen(3000);
 // }
 // bootstrap();
-
-
-
-
