69ab84ff0c51e1aa034bbed651640c9feee57056
duuliy <715181149@qq.com>
2020-08-10 00:20:13 +0800

    feat:nest-mongoDB

--- nest/src/database/mysql.providers.ts
+++ nest/src/database/mysql.providers.ts
@@ -17,3 +17,18 @@ export const databaseProviders = [
       }),
   },
 ];
+
+// mongodb 使用方法
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("runoob");
+//     var myobj = { name: "duuliy", url: "www.runoob" };
+//     dbo.collection("site").insertOne(myobj, function(err, res) {
+//         if (err) throw err;
+//         console.log("文档插入成功");
+//         db.close();
+//     });
+// });
