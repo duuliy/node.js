72c4d52bb4592afd266d4bfe10b2bda6869f4239
zhongyuhan <715181149@qq.com>
2020-09-17 17:45:49 +0800

    feat: 环境变量

--- egg/config/config.prod.js
+++ egg/config/config.prod.js
@@ -15,32 +15,10 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1599533994275_6035';
 
-  // add your middleware config here
-  config.middleware = [];
-
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
-  config.news = {
-    pageSize: 5,
-    serverUrl: 'https://hacker-news.firebaseio.com/v0',
-  };
-
-  config.middleware = [
-    'robot',
-  ];
-  config.robot = {
-    ua: [
-      /Baiduspider/i,
-    ],
-  };
-
   config.env = 'prod';
 
 
   return {
     ...config,
-    ...userConfig,
   };
 };
